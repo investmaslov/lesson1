f4249d1ccdf6e2eb0af077d32007118bf9d9a815
Vladimir <investmaslov@gmail.com>
2020-06-02 15:50:48 +0300

    HomeWork3 Random Game

--- src/main/java/lesson1/HomeWork3.java
+++ src/main/java/lesson1/HomeWork3.java
@@ -8,9 +8,13 @@ import java.util.Random;
 public class HomeWork3 {
     public static void main(String[] args) {
 
+
+
         randomGame();
 
         newStartRandomGame();
+
+        start();
     }
 
 /*  1. Написать программу, которая загадывает случайное число от 0 до 9,
@@ -21,46 +25,62 @@ public class HomeWork3 {
 */
 
 
+    public static void start() {
+        System.out.println("Вы ввели некорректный ответ! Выберите правильный ответ! Спасибо!");
+        newStartRandomGame();
+    }
+
+
     public static void randomGame() {
         System.out.println("Давай поиграем в игру Random Game ?");
         System.out.println("Я загадал число от 0 до 9 угадай какое ? У тебя есть три попытки ");
         Random rand = new Random();
         int x = rand.nextInt(9);
-     //   System.out.println("Программа загадала число - " + x);
+
+        System.out.println("Программа загадала число - " + x);
+
         System.out.println("   ");
+
+        //программа на этом этапе просит пользователя ввести свое число
+
         for (int i = 0; i < 3; i++) {
-            System.out.println("Введите свое число: ");
+            System.out.println("Введите свой вариант числа: ");
             Scanner sc = new Scanner(System.in);
             int a = sc.nextInt();
+
+            //программа на этом этапе сравнивает два числа x и a
             if (a == x) {
                 System.out.println(" Ура ты угадал программа загадала " + x + " " + "Твой результат такой же -  " + a);
                 newStartRandomGame();
             } else if (a < x) {
-                System.out.println("Неправильно! Число " + a +", которое вы указали МЕНЬШЕ , чем то которое загадала программа ");
+                System.out.println("Неправильно! Число " + a + ", которое вы указали МЕНЬШЕ , чем то которое загадала программа ");
+
             } else if (a > x) {
                 System.out.println("Неправильно! Число " + a + ", которое вы указали БОЛЬШЕ  , чем то, которое загадала программа ");
             }
-
-
-
         }
-        System.out.println();
-    }
 
+
+    }
 
 
     public static void newStartRandomGame() {
 
-        System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет");
+        System.out.println("Начать или Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет");
         System.out.println("Введите свой ответ?  ");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        if (a == 1) {
+        if (b == 1) {
             randomGame();
-        } else if (a == 0) {
+        } else if (b == 0) {
             System.out.println("Игра закончена благодарим за игру!");
+        } else if (b < 1) {
+
+            start();
         }
+
+
     }
 }
 
