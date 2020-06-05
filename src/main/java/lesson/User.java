61d3ffb7f760baaedf9700d0787049e50fb1a8e7
Vladimir <investmaslov@gmail.com>
2020-06-05 20:58:18 +0300

    ООП

---
+++ src/main/java/lesson/User.java
@@ -0,0 +1,10 @@
+package lesson;
+
+public class User {
+    public int id;
+    private String name;
+    private String position;
+    private int age;
+}
+
+
--- src/main/java/lesson/main.java
+++ src/main/java/lesson/main.java
@@ -1,13 +1,11 @@
 package lesson;
 
-import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-
-
 
     }
+
 }
+
 
